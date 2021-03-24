b968b8605aa69192895f3d170f8b73dd1c94daca
danielfireman-ifal <daniel.fireman@ifal.edu.br>
2021-03-24 23:37:51 +0000

    Renomeando servidor e alterando cliente

--- client/main.go
+++ client/main.go
@@ -2,23 +2,36 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"net"
 )
 
+var port = flag.String("server", "127.0.0.1:3000", "Host:Porta do servidor UDP")
+var msg = flag.String("msg", "Olá", "Mensagem a ser enviada para o servidor")
+
 func main() {
-	buf := make([]byte, 1024)
-	conn, err := net.Dial("udp", "255.255.255.255:3000")
+	flag.Parse()
+	
+
+	// Abrindo conexão
+	conn, err := net.Dial("udp", *port)
 	if err != nil {
-		fmt.Printf("Some error %v", err)
+		fmt.Printf("Erro: %v", err)
 		return
 	}
-	fmt.Fprintf(conn, "Hi UDP Server, How are you doing?")
+	defer conn.Close()
+
+	// Enviando mensagem.
+	fmt.Fprintf(conn, *msg)
+
+	// Resposta da conexão.
+	buf := make([]byte, 1024)
 	_, err = bufio.NewReader(conn).Read(buf)
 	if err == nil {
 		fmt.Printf("%s\n", string(buf))
 	} else {
-		fmt.Printf("Some error %v\n", err)
+		fmt.Printf("Erro: %v\n", err)
 	}
 	conn.Close()
 }
